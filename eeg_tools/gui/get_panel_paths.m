70d27443a9d87334b02dd415af3bf2f1bc2b1848
inuggi <albaspazio@users.noreply.github.com>
2015-03-03 16:12:07 +0000

    GUI done with Netbeans

--- eeg_tools/gui/get_panel_paths.m
+++ eeg_tools/gui/get_panel_paths.m
@@ -6,10 +6,10 @@ function [] = get_panel_paths(tabbed_panel)
     
     import javax.swing.JPanel
     % The path of the icon "open folder"
-    icon_folder_path = strcat(GlobalData.path_icons, 'iconFolderOpen.jpg');
+    %icon_folder_path = strcat(GlobalData.path_icons, 'iconFolderOpen.jpg');
 
     panel = JPanel();
-    tabbed_panel.addTab('paths', panel);
+    tabbed_panel.addTab('input paths', panel);
 
     % Panel boxlayout.
     % The panel will have a vertical box layout.
@@ -21,126 +21,178 @@ function [] = get_panel_paths(tabbed_panel)
     panel.setLayout(grid);
 
     %% Set project path widgets.
-    tooltip_text = 'B9:   folder containing data, epochs, results etc...(not scripts)';
-    panel = add_path_project_widgets(panel, icon_folder_path, tooltip_text);
-    %% Set the original data path
-    tooltip_text = 'B10:  folder containing EEG raw data (BDF, vhdr, eeg, etc...)';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B11:  folder containing EEGLAB EEG input epochs set files';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B12:  folder containing EEGLAB EEG output condition epochs set files';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B13:  folder containing statistical results';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B14:  folder containing EEGLAB EMG epochs set files';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B15:  folder containing EMG data strucuture';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B16:  folder containing ';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B17:  folder containing ';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B18:  folder containing bash batches (usually for SPM analysis)';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B19:  folder containing sources images exported by brainstorm';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B20:  folder containing spm stat files';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B21:  folder containing spm toolbox';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B22:  folder containing eeglab toolbox';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
-    tooltip_text = 'B23:  folder containing brainstorm toolbox';
-    panel = add_path_original_data_widgets(panel, icon_folder_path, tooltip_text);
+    label = 'Project';
+    label_tooltip = 'B9: folder containing data, epochs, results etc...(not scripts)';
+    [panel, GlobalData.textFields.project.paths.project] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.project, {@searchPath, label});
+        
+    label = 'Original data';
+    label_tooltip = 'B10: folder containing EEG raw data (BDF, vhdr, eeg, etc...)';
+    [panel, GlobalData.textFields.project.paths.original_data] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.original_data, {@searchPath, label});
+
+    label = 'Input epochs';
+    label_tooltip = 'B11: folder containing EEGLAB EEG input epochs set files';
+    [panel, GlobalData.textFields.project.paths.input_epochs] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.input_epochs, {@searchPath, label});
+
+    label = 'Output epochs';
+    label_tooltip = 'B12: folder containing EEGLAB EEG output condition epochs set files';
+    [panel, GlobalData.textFields.project.paths.output_epochs] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.output_epochs, {@searchPath, label});
+
+    label = 'Results';
+    label_tooltip = 'B13: folder containing statistical results';
+    [panel, GlobalData.textFields.project.paths.results] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.results, {@searchPath, label});
+
+    label = 'Emg epochs';
+    label_tooltip = 'B14: folder containing EEGLAB EMG epochs set files';
+    [panel, GlobalData.textFields.project.paths.emg_epochs] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.emg_epochs, {@searchPath, label});
+        
+    label = 'Emg epochs mat';
+    label_tooltip = 'B15: folder containing EMG data strucuture';
+    [panel, GlobalData.textFields.project.paths.emg_epochs_mat] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.emg_epochs_mat, {@searchPath, label});
+        
+    label = 'TF';
+    label_tooltip = 'B16: folder containing ';
+    [panel, GlobalData.textFields.project.paths.tf] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.tf, {@searchPath, label});
+
+    label = 'Cluster projection erp';
+    label_tooltip = 'B17: folder containing ';
+    [panel, GlobalData.textFields.project.paths.cluster_projection_erp] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.cluster_projection_erp, {@searchPath, label});
+
+    label = 'Batches';
+    label_tooltip = 'B18: folder containing bash batches (usually for SPM analysis)';
+    [panel, GlobalData.textFields.project.paths.batches] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.batches, {@searchPath, label});
+        
+    label = 'Spmsources';
+    label_tooltip = 'B19:  folder containing sources images exported by brainstorm';
+    [panel, GlobalData.textFields.project.paths.spmsources] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.spmsources, {@searchPath, label});
+       
+    label = 'Spmstats';
+    label_tooltip = 'B20:  folder containing spm stat files';
+    [panel, GlobalData.textFields.project.paths.spmstats] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.spmstats, {@searchPath, label});
+            
+    label = 'Spm';
+    label_tooltip = 'B21:  folder containing spm toolbox';
+    [panel, GlobalData.textFields.project.paths.spm] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.spm, {@searchPath, label});
+               
+    label = 'Eeglab';
+    label_tooltip = 'B22:  folder containing eeglab toolbox';
+    [panel, GlobalData.textFields.project.paths.eeglab] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.eeglab, {@searchPath, label});
+               
+    label = 'Brainstorm';
+    label_tooltip = 'B23:  folder containing brainstorm toolbox';
+    [panel, GlobalData.textFields.project.paths.brainstorm] = ...
+    add_path_widget(panel, label, label_tooltip, ...
+            GlobalData.project.paths.brainstorm, {@searchPath, label});
     
     GlobalData.top_panel.add(tabbed_panel, java.awt.BorderLayout.CENTER);
     GlobalData.main_tabs.paths = panel;
 
 end
 
-function panel = add_path_project_widgets(panel, icon_folder_path, label_tooltip_text)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [panel, path_text_field]  = add_path_widget(panel, label_txt, label_tooltip_text, path_data, function_handle)
     global GlobalData
 
     % LABEL
-    label = getLabel('Project path: ');
-    label.setToolTipText(label_tooltip_text);
-    
-    % TEXT FIELD
-    GlobalData.textFields.project.path.project = javax.swing.JTextField(...
-                        GlobalData.project.paths.project, 50);
-
-    %BUTTON WITH ICON
-    %With the button the user can choose a different folder.
-    icon_folder_path = strcat(GlobalData.path_icons, 'iconFolderOpen.jpg');
-    button = getButtonWithIcon(icon_folder_path, 0.25, 'ActionPerformedCallback', @searchProjectPath);
-    button.setToolTipText('Choose the path');
-    panel.add(label);
-    panel.add(GlobalData.textFields.project.path.project);
-    panel.add(button);   
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-end
-
-function panel = add_path_original(panel, label_string, icon_folder_path, label_tooltip_text)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    global GlobalData
-
-    % LABEL
+    label_string = strcat(label_txt, ' path:');
     label = getLabel(label_string);
     label.setToolTipText(label_tooltip_text);
     
     % TEXT FIELD
-    GlobalData.textFields.project.paths.original_data = javax.swing.JTextField(...
-                        GlobalData.project.paths.original_data, 50);
+    path_text_field = javax.swing.JTextField(path_data, 50);
 
     %BUTTON WITH ICON
     %With the button the user can choose a different folder.
     icon_folder_path = strcat(GlobalData.path_icons, 'iconFolderOpen.jpg');
-    button = getButtonWithIcon(icon_folder_path, 0.25, 'ActionPerformedCallback', @searchOriginalDataPath);
+    button = getButtonWithIcon(icon_folder_path, 0.25, 'ActionPerformedCallback', function_handle);
     button.setToolTipText('Choose the path');
     panel.add(label);
-    panel.add(GlobalData.textFields.project.paths.original_data);
+    panel.add(path_text_field);
     
-    panel.add(button);   
-end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-function panel = add_path_original_data_widgets(panel, icon_folder_path, label_tooltip_text)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    global GlobalData
-
-    % LABEL
-    label = getLabel('Original Data: ');
-    label.setToolTipText(label_tooltip_text);
+    panel.add(button); 
     
-    % TEXT FIELD
-    GlobalData.textFields.project.paths.original_data = javax.swing.JTextField(...
-                        GlobalData.project.paths.original_data, 50);
+end
 
-    %BUTTON WITH ICON
-    %With the button the user can choose a different folder.
-    icon_folder_path = strcat(GlobalData.path_icons, 'iconFolderOpen.jpg');
-    button = getButtonWithIcon(icon_folder_path, 0.25, 'ActionPerformedCallback', @searchOriginalDataPath);
-    button.setToolTipText('Choose the path');
-    panel.add(label);
-    panel.add(GlobalData.textFields.project.paths.original_data);
+function [dir] = searchPath(varargin)
+    dir = uigetdir('c:\', 'Please select the path');
+    global GlobalData
     
-    panel.add(button);   
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if nargin == 3 % the 3rd argument is the path_type.
+        path_type = varargin{3};
+        
+        switch path_type
+            case 'Project'
+                GlobalData.textFields.project.paths.project.setText(dir);
+                GlobalData.project.paths.project = dir;
+            case 'Original data'
+                GlobalData.textFields.project.paths.original_data.setText(dir);
+                GlobalData.project.paths.original_data = dir;
+            case 'Input epochs'
+                GlobalData.textFields.project.paths.input_epochs.setText(dir);
+                GlobalData.project.paths.input_epochs = dir;
+            case 'Output epochs'
+                GlobalData.textFields.project.paths.output_epochs.setText(dir);
+                GlobalData.project.paths.output_epochs = dir;
+            case 'Results'
+                GlobalData.textFields.project.paths.results.setText(dir);
+                GlobalData.project.paths.results = dir;                
+            case 'Emg epochs'
+                GlobalData.textFields.project.paths.emg_epochs.setText(dir);
+                GlobalData.project.paths.emg_epochs = dir;                
+            case 'Emg epochs mat'
+                GlobalData.textFields.project.paths.emg_epochs_mat.setText(dir);
+                GlobalData.project.paths.emg_epochs_mat = dir;                
+            case 'TF'
+                GlobalData.textFields.project.paths.tf.setText(dir);
+                GlobalData.project.paths.tf = dir;                
+            case 'Cluster projection erp'
+                GlobalData.textFields.project.paths.cluster_projection_erp.setText(dir);
+                GlobalData.project.paths.cluster_projection_erp = dir;                
+            case 'Batches'
+                GlobalData.textFields.project.paths.batches.setText(dir);
+                GlobalData.project.paths.batches = dir;                
+            case 'Spmsources'
+                GlobalData.textFields.project.paths.spmsources.setText(dir);
+                GlobalData.project.paths.spmsources = dir;                
+            case 'Spmstats'
+                GlobalData.textFields.project.paths.spmstats.setText(dir);
+                GlobalData.project.paths.spmstats = dir;                
+            case 'Spm'
+                GlobalData.textFields.project.paths.spm.setText(dir);
+                GlobalData.project.paths.spm = dir;                
+            case 'Eeglab'
+                GlobalData.textFields.project.paths.eeglab.setText(dir);
+                GlobalData.project.paths.eeglab = dir;                
+            case 'Brainstorm'
+                GlobalData.textFields.project.paths.brainstorm.setText(dir);
+                GlobalData.project.paths.brainstorm = dir;                
+        end
+    end
 end
 
-function searchProjectPath(varargin)
-    global GlobalData
-    GlobalData.project.paths.project = uigetdir('c:\', 'Please select the path');  % String
-    GlobalData.textFields.project.path.project.setText(GlobalData.project.paths.project);  % widget
-    disp(GlobalData.project.paths.project)
-end
-
-function searchOriginalDataPath(varargin)
-    global GlobalData
-    GlobalData.project.paths.original_data = uigetdir('c:\', 'Please select the path');
-    GlobalData.textFields.project.path.original_data.setText(GlobalData.project.paths.original_data);
-    disp(GlobalData.project.paths.original_data)
-end
--- eeg_tools/gui/gui.m
+++ eeg_tools/gui/gui.m
@@ -11,121 +11,48 @@ function [] = gui()
     % Initialise the global variable struct.
     global GlobalData;
     GlobalData = initialise_params('GlobalData');
-
-    %template_main_eeglab_subject_processing
-    %template_main_eeglab_group_processing
+ 
+    project.name = 'gigi';
     template_project_structure
 
     if exist('project', 'var')
-        fill_global_data_with_template(project)
+        fill_global_data_with_template(project);
     end
- 
+
     %%
-    % Create the main frame.
+    % Create the main frame and the related TABs.
     GlobalData.main_panel = createMainPanel();
-    main_panel = GlobalData.main_panel;
-
-    %%
-    % create the top panel 
-    % Need a top panel to ad the the main frame.
     GlobalData.top_panel = createTopPanel();
-    top_panel =  GlobalData.top_panel;
+    GlobalData.tabbed_panel = addTabbedPanel();
+    GlobalData.top_panel.add(GlobalData.tabbed_panel);
 
-    %% The tabbed panel can only be added to a panel not to the main frame.
-    tabbed_panel = addTabbedPanel();
+    button_panel = addPanelToTabbedPanel(GlobalData.tabbed_panel, 'Start');
+    GlobalData.tabbed_panel.addTab('Start', button_panel);
+
+    %%%%% NEW PROJECT TAB
+    % Add the button 'NEW PROJECT'
+    jbutton = javax.swing.JButton('NEW PROJECT');
+    button_handle = handle(jbutton,'CallbackProperties');
+    set(button_handle, 'ActionPerformedCallback', @new_project);
     
-    %% Panel with paths.
-    get_panel_paths(tabbed_panel);
-    
-    %%
-    % Create now the TABs
-    
-%     GlobalData.main_tabs = [GlobalData.main_tabs addPanelToTabbedPanel(tabbed_panel, 'Panel 1')];
-%     GlobalData.main_tabs = [GlobalData.main_tabs addPanelToTabbedPanel(tabbed_panel, 'Panel 2')];
-%     GlobalData.main_tabs = [GlobalData.main_tabs addPanelToTabbedPanel(tabbed_panel, 'Panel 3')];
-%     
-    % TAB 1 -grid box
-%     bf = javax.swing.BorderFactory;
-%     border = javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5);
-%     GlobalData.main_tabs(1).setBorder(border);
-%     % 
-%     % GridLayout(rows, cols, x_border, y_border)
-%     grid = java.awt.GridLayout(10, 4, 10, 10);
-%     GlobalData.main_tabs(1).setLayout(grid);
-%     buttons = {...
-%             'Cls', 'Bck', '', 'Close', '7', '8', '9', '/', '4',...
-%             '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'...
-%         };
-%     for i=1:length(buttons)
-%         if i == 2
-%             label = javax.swing.JLabel(buttons{i}, javax.swing.SwingConstants.CENTER);
-%             GlobalData.main_tabs(1).add(label);
-%         else
-%             button = javax.swing.JButton(buttons{i});
-%             GlobalData.main_tabs(1).add(button);
-%         end
-%     end
-%     
-%     % TAB 2
-%     GlobalData.main_tabs(2) = addLabelToPanel(GlobalData.main_tabs(2), 'this is a test');
-%     GlobalData.main_tabs(2) = addButtonToPanel(GlobalData.main_tabs(2), 'test button');
-% 
-%     % TEXT FIELD
-%     GlobalData.textFields.path = javax.swing.JTextField('default', 50);
-%     GlobalData.main_tabs(2).add(GlobalData.textFields.path, java.awt.BorderLayout.CENTER);
-%     GlobalData.textFields.path.setText('Control in action: JTextField');
-% %     str = GlobalData.textFields.path.getText();
-% %     disp(str)
-%     
-%     % button
-%     button = javax.swing.JButton('invio');
-%     GlobalData.main_tabs(2).add(button);
-%     %set(panel,'UserData',params); 
-%     button_handle = handle(button,'CallbackProperties');
-%     set(button_handle, 'ActionPerformedCallback', @enterPath);
-%     
-%     top_panel.add(tabbed_panel, java.awt.BorderLayout.CENTER);
-%     
-%     %%%%% TAB 3 -box layout
-%     Y_AXIS = javax.swing.BoxLayout.Y_AXIS;
-%     % We say that GlobalData.main_tabs(3) has vertical box layout.
-%     % With this I make tab 3 as a vertical box layout.
-%     boxlayout = javax.swing.BoxLayout(GlobalData.main_tabs(3), Y_AXIS);
-%     GlobalData.main_tabs(3).setLayout(boxlayout);
-%
-%     bottom = javax.swing.JPanel(); %java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-%     % The bottom panel is right aligned. 
-% %    bottom.setAlignmentX(1);
-% %    bottom.setAlignmentX(java.awt.Component.CENTER_ALIGNMENT);
-%     X_AXIS = javax.swing.BoxLayout.X_AXIS;
-%     % We say that bottom has horizontal box layout
-%     boxlayout = javax.swing.BoxLayout(bottom, X_AXIS);
-%     bottom.setLayout(boxlayout);
-% 
-%     % Look at ButtonGroup radioGroup = new ButtonGroup();
-%     
-%     ok = javax.swing.JButton('OK');
-%     close = javax.swing.JButton('Close');
-% 
-%     bottom.add(ok);
-%     %rigidarea1 = javax.swing.Box.createRigidArea(java.awt.Dimension(5, 0));
-% %      bottom.add(javax.swing.Box.createRigidArea(java.awt.Dimension(5, 0)));
-%     bottom.add(close);
-%     %rigidarea2 = javax.swing.Box.createRigidArea(java.awt.Dimension(15, 0));
-% %      bottom.add(javax.swing.Box.createRigidArea(java.awt.Dimension(0, 0)));
-%     GlobalData.main_tabs(3).add(bottom); %, java.awt.BorderLayout.CENTER);
-%                   %rigidarea3 = javax.swing.Box.createRigidArea(java.awt.Dimension(0, 15));
-%     GlobalData.main_tabs(3).add(javax.swing.Box.createRigidArea(java.awt.Dimension(0, 0)));
-%     %%%%%% END TAB 3
+    Y_AXIS = javax.swing.BoxLayout.Y_AXIS;
+    boxlayout = javax.swing.BoxLayout(button_panel, Y_AXIS);
+    verticalGlue = javax.swing.Box.createVerticalGlue();
+
+    button_panel.setLayout(boxlayout);
+    button_panel.add(verticalGlue);
+    button_panel.add(jbutton);
+    % Add later so that the content will always appear in the panel.
+    button_panel.add(javax.swing.Box.createRigidArea(java.awt.Dimension(200, 400)));
     
     % Display the main frame
-    main_panel.setVisible(1);
+    GlobalData.main_panel.setVisible(1);
 
     hMutex = bst_mutex('get', 'gui');
     set(hMutex, 'Visible', 'off'); 
-    %bst_mutex('waitfor', 'gui');
 end
 
+%%
 function main_panel = createMainPanel()
     % It create the main frame of the GUI
     
@@ -143,6 +70,7 @@ function main_panel = createMainPanel()
     %main_panel.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 end
 
+%%
 function top_panel = createTopPanel()
     % The top panel is the panel where all of the other widgets will be
     % laid on.
@@ -152,13 +80,14 @@ function top_panel = createTopPanel()
     GlobalData.main_panel.getContentPane().add(top_panel);
 end
 
-
+%%
 function [tabbed_panel] = addTabbedPanel()
     % This function creates a tabbed panel object. 
     % It returns such an object.
     tabbed_panel = javax.swing.JTabbedPane();
 end
 
+%%
 function [panel] = addPanelToTabbedPanel(tabbed_panel, tab_name)
     % This function adds a tab panel to the main frame.
     % It returns the updated tabbed_panel.
@@ -166,48 +95,37 @@ function [panel] = addPanelToTabbedPanel(tabbed_panel, tab_name)
     tabbed_panel.addTab(tab_name, panel);
 end
 
-function [panel, button_handle] = addButtonToPanel(panel, button_label)
-    % The function adds a label to the panel with the font specified by the
-    % input variable.
-    button = javax.swing.JButton(button_label);
-    button.setBounds(50, 50, 200, 100);
-    panel.add(button);
-    %set(panel,'UserData',params); 
+function new_project(varargin)
+    % This is the callback method which is called when the nre project event 
+    % is raised by pressing the related button.
 
-    button_handle = handle(button,'CallbackProperties');
-    set(button_handle, 'ActionPerformedCallback', @mycallbackmethod);
-    %set(button_handle, 'ActionPerformedCallback', {@mycallbackmethod, param, output}); 
-
-end
-
-function mycallbackmethod(varargin) %parprojInfo, hNewProjDialog)
-    % This is the callback method which is called when the event is raise
-    % by pressing the button
-    % src is the <1x1 java.awt.event.ActionEvent>
-    % evd is the <1x1 javahandle_withcallbacks.javax.swing.JButton>
-    
-    disp('ciao');
-    global GlobalData;
-    GlobalData.button_test.value = 10;
-end
-
-function enterPath(varargin) %parprojInfo, hNewProjDialog)
-    % This is the callback method which is called when the event is raise
-    % by pressing the button
-    % src is the <1x1 java.awt.event.ActionEvent>
-    % evd is the <1x1 javahandle_withcallbacks.javax.swing.JButton>
+    %% The tabbed panel can only be added to a panel not to the main frame.
+    %% Panel with paths.
     global GlobalData
-    var = GlobalData.textFields.path.getText();
-    disp(var);
+    % If the tab is already open send a warning to the user.
+    num_open_tabs = GlobalData.tabbed_panel.getTabCount();
+    if num_open_tabs > 1
+        string = 'A project is already open. Do you want to close the open project?';
+        %javax.swing.JOptionPane.showMessageDialog(GlobalData.main_panel, string);
+        not_close_it = javax.swing.JOptionPane.showConfirmDialog(GlobalData.main_panel, string, ...
+            'Warning', ...
+            javax.swing.JOptionPane.YES_NO_OPTION);
+        
+        % if user wants to close kill all the tabs except the first one.
+        if not_close_it == 0
+            for i=(num_open_tabs-1):-1:1
+                GlobalData.tabbed_panel.remove(i)
+            end
+        end
+        return
+    end
+
+    % LOAD ALL THE PANELS.
+    display_panel_paths(GlobalData.tabbed_panel)
+    get_panel_paths(GlobalData.tabbed_panel);
+    % The following index is the number of the panel we would like to open
+    % afer the new peoject button has been pressed.
+    index = 1;
+    GlobalData.tabbed_panel.setSelectedIndex(index); 
+    %GlobalData.main_panel.setVisible(1);
 end
-
-
-
-% function [output] = mycallbackmethod(src, evd, param, output) %parprojInfo, hNewProjDialog)
-%     % This is the callback method which is called when the event is raise
-%     % by pressing the button
-%     % src is the <1x1 java.awt.event.ActionEvent>
-%     % evd is the <1x1 javahandle_withcallbacks.javax.swing.JButton>
-%     disp('ciao')
-%     output = param * param;
-% end
