fbfc87c50bb1669122f97eb497f487a1f9bc642d
goccia <goccia@12a1f503-7b73-4d02-8271-4b1246477144>
2015-03-09 11:00:10 +0000

    cleaned the code.

--- CommonScript/eeg/eeg_tools/gui/gui.m
+++ CommonScript/eeg/eeg_tools/gui/gui.m
@@ -1,36 +1,22 @@
 function [] = gui()
 %GUI Summary of this function goes here
 %   Detailed explanation goes here
-%
-     % MATLAB_JAVA
-     % C:\Program Files (x86)\Java\jdk1.8.0_40\jre
 
-    javaaddpath('C:\projects\behaviourPlatform\CommonScript\eeg\eeg_tools\gui\eeggui\dist\eeggui.jar')
-%    javaaddpath('C:\Users\goccia\Documents\NetBeansProjects\eeggui\dist\eeggui.jar')
-%    javaaddpath([pwd filesep 'eeggui.jar'])
+    javaj_jar_path = '\eeggui\dist\eeggui.jar';
+    javaaddpath(javaj_jar_path)
+
     global gg  % Make the handle to the main panel global
 
-    %fig = figure('visible', 'on');
     fig = figure('visible', 'on', 'units','normalized','outerposition',[0 0 1 1]);
     drawnow
-    %frame = GXJFrame(fig);
     gg = GImport(fig, gui.basePanel());
-
-% %    g = GImport(frame, gui.basePanel(), false);
-%     %gg=GImport(figure(1), gui.basePanel());
     gg.getHandles();
-%     
-%     %a = g.getComponents();
-%     %jbutton = a{10};  % More complicated
-%     %g.setCallback(jbutton, 'ActionPerformedCallback', @callback_print)
-%     %textfield = jbutton.name;
-%     
-%
-     newButton = gg.getHandles().New;
-%     newButton = gg.find('New');   % Much easier to access the button.
-     gg.setCallback(newButton, 'ActionPerformedCallback', @print_string, 'marcello')
 
-%    clear java       
-%
+    newButton = gg.getHandles().New;
+    gg.setCallback(newButton, 'ActionPerformedCallback', @print_string, 'marcello')
+    
+%      if exist('gg', 'var')
+%         javarmpath(javaj_jar_path)
+%     end
 end
 
