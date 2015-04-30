7de419987705fad3ecd1b197fab559ef4a47d7ff
campus <campus@12a1f503-7b73-4d02-8271-4b1246477144>
2015-04-30 09:35:26 +0000

    rilascio onset offset

--- CommonScript/eeg/eeglab/utility/export/text/text_export_ersp_onset_offset_sub_continuous_struct.m
+++ CommonScript/eeg/eeglab/utility/export/text/text_export_ersp_onset_offset_sub_continuous_struct.m
@@ -1,7 +1,7 @@
-%% function [dataexpcols, dataexp] = text_export_erp_continuous_struct(out_file,erp_struct)
+%% function [dataexpcols, dataexp] = text_export_ersp_continuous_struct(out_file,ersp_struct)
 % export from matlab structure in tab delimited text format data for further statistics.
 
-function [dataexpcols, dataexp] = text_export_erp_onset_offset_sub_continuous_struct(out_file,erp_struct)
+function [dataexpcols, dataexp] = text_export_ersp_onset_offset_sub_continuous_struct(out_file,ersp_struct)
 
 
 %% inizialize
@@ -18,49 +18,56 @@ cell_pvec_corrected                                                        = [];
 cell_pvec_raw                                                              = [];
 cell_sigvec                                                                = [];
 cell_times                                                                 = [];
-cell_sub                                                                   = []; 
+cell_sub                                                                   = [];
 cell_roi                                                                   = [];
 cell_f1                                                                    = [];
 cell_f2                                                                    = [];
+cell_band                                                                  = [];
+
 
 if nargin < 2
-    help text_export_erp_onset_offset_sub_continuous_struct;
+    help text_export_ersp_onset_offset_sub_continuous_struct;
     return;
 end;
 
 
-for nroi=1:length(erp_struct.roi_names) % for each roi
-    for nl1=1:length(erp_struct.study_des.variable(1).value) % for each level of factor 1
-        for nl2=1:length(erp_struct.study_des.variable(2).value) % for each level of factor 2
-            sub_list = erp_struct.list_design_subjects{nl1,nl2};
-            for nsub=1:length(sub_list) % for each subject
-                data = erp_struct.dataroi(nroi).datatw.onset_offset.sub{nl1,nl2,nsub}.continuous;
-                
-                if not(isempty(data))
-                    curve                                                      = data.curve;
-                    pvec_corrected                                             = data.pvec_corrected;
-                    pvec_raw                                                   = data.pvec_raw;
-                    sigvec                                                     = data.sigvec;
-                    times                                                      = data.times;
+for nroi=1:length(ersp_struct.roi_names) % for each roi
+    for nband=1:length(ersp_struct.frequency_bands_names) % for each frequency band
+        for nl1=1:length(ersp_struct.study_des.variable(1).value) % for each level of factor 1
+            for nl2=1:length(ersp_struct.study_des.variable(2).value) % for each level of factor 2
+                sub_list = ersp_struct.list_design_subjects{nl1,nl2};                
+                for nsub=1:length(sub_list) % for each subject
+                    data = ersp_struct.dataroi(nroi).databand(nband).datatw.onset_offset.sub{nl1,nl2,nsub}.continuous;
                     
-                    lc                                                         = length(curve);
+                    if not(isempty(data))
+                        curve                                                      = data.curve;
+                        pvec_corrected                                             = data.pvec_corrected;
+                        pvec_raw                                                   = data.pvec_raw;
+                        sigvec                                                     = data.sigvec;
+                        times                                                      = data.times;
+                        
+                        lc                                                         = length(curve);
+                        
+                        roi                                                        = repmat(ersp_struct.roi_names(nroi),lc,1);
+                        f1                                                         = repmat(ersp_struct.study_des.variable(1).value(nl1),lc,1);
+                        f2                                                         = repmat(ersp_struct.study_des.variable(2).value(nl2),lc,1);
+                        band                                                       = repmat(ersp_struct.frequency_bands_names(nband),lc,1);
+
+                        sub                                                        = repmat(sub_list(nsub),lc,1);
+                    end
                     
-                    roi                                                        = repmat(erp_struct.roi_names(nroi),lc,1);
-                    f1                                                         = repmat(erp_struct.study_des.variable(1).value(nl1),lc,1);
-                    f2                                                         = repmat(erp_struct.study_des.variable(2).value(nl2),lc,1);
-                    sub                                                        = repmat(sub_list(nsub),lc,1);                                    
+                    cell_curve                                                     = [cell_curve;                curve];
+                    cell_pvec_corrected                                            = [cell_pvec_corrected;       pvec_corrected];
+                    cell_pvec_raw                                                  = [cell_pvec_raw;             pvec_raw];
+                    cell_sigvec                                                    = [cell_sigvec;               sigvec];
+                    cell_times                                                     = [cell_times,                times];
+                    
+                    cell_sub                                                       = [cell_sub; sub];
+                    cell_roi                                                       = [cell_roi; roi];
+                    cell_f1                                                        = [cell_f1; f1];
+                    cell_f2                                                        = [cell_f2; f2];
+                    cell_band                                                      = [cell_band; band];
                 end
-                
-                cell_curve                                                     = [cell_curve;                curve];
-                cell_pvec_corrected                                            = [cell_pvec_corrected;       pvec_corrected];
-                cell_pvec_raw                                                  = [cell_pvec_raw;             pvec_raw];
-                cell_sigvec                                                    = [cell_sigvec;               sigvec];
-                cell_times                                                     = [cell_times,                times];
-                
-                cell_sub                                                       = [cell_sub; sub];
-                cell_roi                                                       = [cell_roi; roi];
-                cell_f1                                                        = [cell_f1; f1];
-                cell_f2                                                        = [cell_f2; f2];
             end
         end
     end
@@ -74,37 +81,8 @@ cell_sigvec           = num2cell(cell_sigvec);
 
 
 
-if isempty(char(erp_struct.study_des.variable(1).label))
-    dataexpcols={ char(erp_struct.study_des.variable(2).label) ,  'roi', 'sub',...
-        'times',    'curve',                    'pvec_raw',    'pvec_corrected',                  'sigvec', ...
-        };
-    
-    formatSpecCols = [repmat('%s\t',1,length(dataexpcols)-1),'%s\r\n'];
-    
-    formatSpecData = '%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\r\n';
-    
-    
-    dataexp=[cell_f2,                    cell_roi, cell_sub,...
-        cell_times, cell_curve,                 cell_pvec_raw,    cell_pvec_corrected,              cell_sigvec,...
-        ];
-    
-elseif isempty(char(erp_struct.study_des.variable(2).label))
-    
-    dataexpcols={ char(erp_struct.study_des.variable(1).label) , 'roi', 'sub',...
-        'times',   'curve',                    'pvec_raw',    'pvec_corrected',                  'sigvec', ...
-        };
-    
-    formatSpecCols = [repmat('%s\t',1,length(dataexpcols)-1),'%s\r\n'];
-    
-    formatSpecData =  '%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\r\n';
-    
-    
-    dataexp=[cell_f1,                    cell_roi, cell_sub,...
-        cell_times, cell_curve,                 cell_pvec_raw,    cell_pvec_corrected,              cell_sigvec,...
-        ];
-    
-else
-    dataexpcols={ char(erp_struct.study_des.variable(1).label), char(erp_struct.study_des.variable(2).label),'roi', 'sub',...
+if isempty(char(ersp_struct.study_des.variable(1).label))
+    dataexpcols={ char(ersp_struct.study_des.variable(2).label) ,  'roi','band' ,'sub',...
         'times',    'curve',                    'pvec_raw',    'pvec_corrected',                  'sigvec', ...
         };
     
@@ -112,12 +90,41 @@ else
     
     formatSpecData = '%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\r\n';
     
-    dataexp=[                      cell_f1,                      cell_f2,                       cell_roi, cell_sub,...
+    
+    dataexp=[cell_f2,                    cell_roi,cell_band, cell_sub,...
+        cell_times, cell_curve,                 cell_pvec_raw,    cell_pvec_corrected,              cell_sigvec,...
+        ];
+    
+elseif isempty(char(ersp_struct.study_des.variable(2).label))
+    
+    dataexpcols={ char(ersp_struct.study_des.variable(1).label) , 'roi','band' , 'sub',...
+        'times',   'curve',                    'pvec_raw',    'pvec_corrected',                  'sigvec', ...
+        };
+    
+    formatSpecCols = [repmat('%s\t',1,length(dataexpcols)-1),'%s\r\n'];
+    
+    formatSpecData =  '%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\r\n';
+    
+    
+    dataexp=[cell_f1,                    cell_roi,cell_band, cell_sub,...
+        cell_times, cell_curve,                 cell_pvec_raw,    cell_pvec_corrected,              cell_sigvec,...
+        ];
+    
+else
+    dataexpcols={ char(ersp_struct.study_des.variable(1).label), char(ersp_struct.study_des.variable(2).label),'roi','band' , 'sub',...
+        'times',    'curve',                    'pvec_raw',    'pvec_corrected',                  'sigvec', ...
+        };
+    
+    formatSpecCols = [repmat('%s\t',1,length(dataexpcols)-1),'%s\r\n'];
+    
+    formatSpecData = '%s\t%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\r\n';
+    
+    dataexp=[                      cell_f1,                      cell_f2,                       cell_roi, cell_band,cell_sub,...
         cell_times, cell_curve,           cell_pvec_raw,                cell_pvec_corrected,           cell_sigvec,...
         ];
 end
 
-%     out_file = fullfile(plot_dir,'erp_topo-stat.txt');
+%     out_file = fullfile(plot_dir,'ersp_topo-stat.txt');
 fileID = fopen(out_file,'w');
 fprintf(fileID,formatSpecCols,dataexpcols{:});
 [nrows,ncols] = size(dataexp);
