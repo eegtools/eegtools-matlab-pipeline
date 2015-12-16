6fadeeacf52268e58027af6ef3c1b02277ece49d
inuggi <inuggi@12a1f503-7b73-4d02-8271-4b1246477144>
2015-12-16 11:58:05 +0000

    metto check che esistano più di 2 trial nel file, altrimenti a eeglab sembra un continuo e non mi estrae gli eventi

--- CommonScript/eeg/eeglab/study/proj_eeglab_study_create.m
+++ CommonScript/eeg/eeglab/study/proj_eeglab_study_create.m
@@ -1,4 +1,4 @@
-%% [STUDY, EEG] = proj_eeglab_study_create(project) 
+%% [STUDY, EEG] = proj_eeglab_study_create(project)
 %
 % create an EEGLAB STUDY from a list of datasets.
 %
@@ -7,60 +7,66 @@
 %
 % ----------------------------------------------------------------------------------------------------
 % project; a structure with the following MANDATORY fields:
-% 
+%
 % * project.study.filename; the filename of the study
 % * project.paths.output_epochs; the path where the study file will be
 %   placed (default: the same foleder of the epoched datasets)
-% * project.epoching.condition_names; the names (cell array of strings) of the experimental conditions 
-% * project.subjects.groups; the names (cell array of strings) of the subjetcs' groups 
-% * project.epoching.input_suffix; the suffix denoting the datasets after the dataset-level preprocessing pipeline  
+% * project.epoching.condition_names; the names (cell array of strings) of the experimental conditions
+% * project.subjects.groups; the names (cell array of strings) of the subjetcs' groups
+% * project.epoching.input_suffix; the suffix denoting the datasets after the dataset-level preprocessing pipeline
 % * project.subjects.group_names; the names of the subjects in each group (2d {grp}{subj} cell array)
 % ====================================================================================================
 
 function [STUDY, EEG] = proj_eeglab_study_create(project)
-    if nargin < 1
-        help proj_eeglab_study_create;
-        return;
-    end;
+if nargin < 1
+    help proj_eeglab_study_create;
+    return;
+end;
 
-    study_name          = project.study.filename;
-    epochs_path         = project.paths.output_epochs;
-    condition_names     = project.epoching.condition_names;
-    group_list          = project.subjects.groups;
-    import_out_suffix   = project.import.output_suffix;
-    group_names         = project.subjects.group_names;
-    
-    nset=1;
-    commands={};
+study_name          = project.study.filename;
+epochs_path         = project.paths.output_epochs;
+condition_names     = project.epoching.condition_names;
+group_list          = project.subjects.groups;
+import_out_suffix   = project.import.output_suffix;
+group_names         = project.subjects.group_names;
 
-    study_path  = fullfile(epochs_path, study_name);
-    [study_folder, study_name_noext, study_ext] = fileparts(study_path);    
+nset=1;
+commands={};
 
-    % start EEGLab
-    [ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab;
-    STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
+study_path  = fullfile(epochs_path, study_name);
+[study_folder, study_name_noext, study_ext] = fileparts(study_path);
 
-    %% create the study with the epoched data of all subjects
-    % load each epochs set file (subject and condition) into the study structure
-    % of EEGLab
-    for grp=1:length(group_list)
-        for subj=1:length(group_list{grp})
-            for cond=1:length(condition_names)
+% start EEGLab
+[ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab;
+STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
+
+%% create the study with the epoched data of all subjects
+% load each epochs set file (subject and condition) into the study structure
+% of EEGLab
+for grp=1:length(group_list)
+    for subj=1:length(group_list{grp})
+        for cond=1:length(condition_names)
+            
+            setname=[project.import.original_data_prefix group_list{grp}{subj} project.import.original_data_suffix import_out_suffix project.epoching.input_suffix '_'  condition_names{cond} '.set'];
+            fullsetname=fullfile(epochs_path,setname,'');
+            
+            
+            if exist(fullsetname,'file')
                 
-                setname=[project.import.original_data_prefix group_list{grp}{subj} project.import.original_data_suffix import_out_suffix project.epoching.input_suffix '_'  condition_names{cond} '.set'];
-                fullsetname=fullfile(epochs_path,setname,'');
-                if exist(fullsetname,'file')
+                EEG = pop_loadset(fullsetname);
+                if EEG.trials>1
                     cmd={'index' nset 'load' fullsetname 'subject' group_list{grp}{subj} 'session' 1 'condition' condition_names{cond} 'group' group_names{grp}};
-                    commands=[commands, cmd];    
-                    nset=nset+1;    
+                    commands=[commands, cmd];
+                    nset=nset+1;
                 end
             end
         end
     end
-    [STUDY, ALLEEG] = std_editset( STUDY, ALLEEG, 'name' ,study_name, 'commands',commands,'updatedat','on','savedat','on' );
-    CURRENTSTUDY = 1; EEG = ALLEEG; CURRENTSET = [1:length(EEG)];
-    
-    %% save study on file
-    [STUDY, ALLEEG] = std_checkset(STUDY, ALLEEG);
-    [STUDY, EEG] = pop_savestudy( STUDY, EEG, 'filename',[study_name_noext '.study'],'filepath',study_folder);
+end
+[STUDY, ALLEEG] = std_editset( STUDY, ALLEEG, 'name' ,study_name, 'commands',commands,'updatedat','on','savedat','on' );
+CURRENTSTUDY = 1; EEG = ALLEEG; CURRENTSET = [1:length(EEG)];
+
+%% save study on file
+[STUDY, ALLEEG] = std_checkset(STUDY, ALLEEG);
+[STUDY, EEG] = pop_savestudy( STUDY, EEG, 'filename',[study_name_noext '.study'],'filepath',study_folder);
 end
