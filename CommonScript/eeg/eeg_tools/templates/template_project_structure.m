176bf1548347795819471c1f9a85866b97896ee7
campus <campus@12a1f503-7b73-4d02-8271-4b1246477144>
2016-10-14 13:53:43 +0000

    viene chiamata una funzione che alberto ha immaginato di mettere in futuro ma che poi non ha fatto (almeno io non ce l'ho in locale) e riscongelo il codice corrispondente (è la seconda volta che lo faccio, sempre lo stesso pezzo...)

--- CommonScript/eeg/eeg_tools/templates/template_project_structure.m
+++ CommonScript/eeg/eeg_tools/templates/template_project_structure.m
@@ -1559,7 +1559,7 @@ project.brainstorm.postprocess.numscouts=length(project.brainstorm.postprocess.s
 % ======================================================================================================
 
 
-project = eegtools_project_derive_parameters(project);
+% project = eegtools_project_derive_parameters(project);
 % project = eegtools_project_check(project)
 
 
@@ -1576,66 +1576,66 @@ project = eegtools_project_derive_parameters(project);
 
 
 
-% % ======================================================================================================
-% % ======================================================================================================
-% % ======================================================================================================
-% % DERIVED TIMES from seconds to milliseconds
-% % ======================================================================================================
-% % ======================================================================================================
-% % ======================================================================================================
-% 
-% % ======================================================================================================
-% % EPOCHING
-% % ======================================================================================================
-% 
-% %  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
-% project.epoching.bc_st.ms   = project.epoching.bc_st.s*1000;            % baseline correction start latency
-% project.epoching.bc_end.ms  = project.epoching.bc_end.s*1000;           % baseline correction end latency
-% project.epoching.epo_st.ms  = project.epoching.epo_st.s*1000;             % epochs start latency
-% project.epoching.epo_end.ms = project.epoching.epo_end.s*1000;             % epochs end latency
-% 
-% project.epoching.baseline_mark.baseline_begin_target_marker_delay.ms = project.epoching.baseline_mark.baseline_begin_target_marker_delay.s *1000; % delay  between target and baseline begin marker to be inserted
-% 
-% %  ********* /DERIVED DATA *****************************************************************
-% 
-% 
-% % ======================================================================================================
-% % POSTPROCESS
-% % ======================================================================================================
-% 
-% %  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
-% for fb=1:length(project.postprocess.ersp.frequency_bands)
-%     project.postprocess.eeglab.frequency_bands_list{fb,1}=[project.postprocess.ersp.frequency_bands(fb).min,project.postprocess.ersp.frequency_bands(fb).max];
-% end
-% project.postprocess.eeglab.frequency_bands_names = {project.postprocess.ersp.frequency_bands.name};%  ********* /DERIVED DATA  *****************************************************************
-% 
-% 
-% % ERP
-% %  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
-% project.study.erp.tmin_analysis.ms                  = project.study.erp.tmin_analysis.s*1000;
-% project.study.erp.tmax_analysis.ms                  = project.study.erp.tmax_analysis.s*1000;
-% project.study.erp.ts_analysis.ms                    = project.study.erp.ts_analysis.s*1000;
-% project.study.erp.timeout_analysis_interval.ms      = project.study.erp.timeout_analysis_interval.s*1000;
-% %  ********* /DERIVED DATA  *****************************************************************
-% 
-% 
-% % ERSP
-% %  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
-% project.study.ersp.tmin_analysis.ms                 = project.study.ersp.tmin_analysis.s*1000;
-% project.study.ersp.tmax_analysis.ms                 = project.study.ersp.tmax_analysis.s*1000;
-% project.study.ersp.ts_analysis.ms                   = project.study.ersp.ts_analysis.s*1000;
-% project.study.ersp.timeout_analysis_interval.ms     = project.study.ersp.timeout_analysis_interval.s*1000;
-% %  ********* /DERIVED DATA  *****************************************************************
-% 
-% 
-% % ======================================================================================================
-% % RESULTS DISPLAY
-% % ======================================================================================================
-%       
-% %  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
-% project.results_display.erp.time_range.ms           = project.results_display.erp.time_range.s*1000;
-% project.results_display.ersp.time_range.ms          = project.results_display.ersp.time_range.s*1000;
-% %  ********* /DERIVED DATA  *****************************************************************
+% ======================================================================================================
+% ======================================================================================================
+% ======================================================================================================
+% DERIVED TIMES from seconds to milliseconds
+% ======================================================================================================
+% ======================================================================================================
+% ======================================================================================================
+
+% ======================================================================================================
+% EPOCHING
+% ======================================================================================================
+
+%  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
+project.epoching.bc_st.ms   = project.epoching.bc_st.s*1000;            % baseline correction start latency
+project.epoching.bc_end.ms  = project.epoching.bc_end.s*1000;           % baseline correction end latency
+project.epoching.epo_st.ms  = project.epoching.epo_st.s*1000;             % epochs start latency
+project.epoching.epo_end.ms = project.epoching.epo_end.s*1000;             % epochs end latency
+
+project.epoching.baseline_mark.baseline_begin_target_marker_delay.ms = project.epoching.baseline_mark.baseline_begin_target_marker_delay.s *1000; % delay  between target and baseline begin marker to be inserted
+
+%  ********* /DERIVED DATA *****************************************************************
+
+
+% ======================================================================================================
+% POSTPROCESS
+% ======================================================================================================
+
+%  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
+for fb=1:length(project.postprocess.ersp.frequency_bands)
+    project.postprocess.eeglab.frequency_bands_list{fb,1}=[project.postprocess.ersp.frequency_bands(fb).min,project.postprocess.ersp.frequency_bands(fb).max];
+end
+project.postprocess.eeglab.frequency_bands_names = {project.postprocess.ersp.frequency_bands.name};%  ********* /DERIVED DATA  *****************************************************************
+
+
+% ERP
+%  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
+project.study.erp.tmin_analysis.ms                  = project.study.erp.tmin_analysis.s*1000;
+project.study.erp.tmax_analysis.ms                  = project.study.erp.tmax_analysis.s*1000;
+project.study.erp.ts_analysis.ms                    = project.study.erp.ts_analysis.s*1000;
+project.study.erp.timeout_analysis_interval.ms      = project.study.erp.timeout_analysis_interval.s*1000;
+%  ********* /DERIVED DATA  *****************************************************************
+
+
+% ERSP
+%  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
+project.study.ersp.tmin_analysis.ms                 = project.study.ersp.tmin_analysis.s*1000;
+project.study.ersp.tmax_analysis.ms                 = project.study.ersp.tmax_analysis.s*1000;
+project.study.ersp.ts_analysis.ms                   = project.study.ersp.ts_analysis.s*1000;
+project.study.ersp.timeout_analysis_interval.ms     = project.study.ersp.timeout_analysis_interval.s*1000;
+%  ********* /DERIVED DATA  *****************************************************************
+
+
+% ======================================================================================================
+% RESULTS DISPLAY
+% ======================================================================================================
+      
+%  ********* DERIVED DATA : DO NOT EDIT *****************************************************************
+project.results_display.erp.time_range.ms           = project.results_display.erp.time_range.s*1000;
+project.results_display.ersp.time_range.ms          = project.results_display.ersp.time_range.s*1000;
+%  ********* /DERIVED DATA  *****************************************************************
 
 % ======================================================================================================
 % ======================================================================================================
